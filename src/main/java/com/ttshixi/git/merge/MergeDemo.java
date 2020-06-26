ba3b4cebd6f72506ffb969e71215ee728f7b40de
shiwei8b402@126.com <shiwei8b402@126.com>
2020-06-26 22:17:05 +0800

    push from master

--- src/main/java/com/ttshixi/git/merge/MergeDemo.java
+++ src/main/java/com/ttshixi/git/merge/MergeDemo.java
@@ -3,5 +3,6 @@ package com.ttshixi.git.merge;
 public class MergeDemo {
     public static void main(String[] args) {
         System.out.println("this is a merge demo");
+        System.out.println("I am master");
     }
 }
