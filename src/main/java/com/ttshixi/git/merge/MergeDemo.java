5ba1da2c327c39ac23746f01c0adef9f0af76b91
shiwei8b402@126.com <shiwei8b402@126.com>
2020-06-26 22:49:10 +0800

    init

--- src/main/java/com/ttshixi/git/merge/MergeDemo.java
+++ src/main/java/com/ttshixi/git/merge/MergeDemo.java
@@ -3,6 +3,5 @@ package com.ttshixi.git.merge;
 public class MergeDemo {
     public static void main(String[] args) {
         System.out.println("this is a merge demo");
-        System.out.println("I am master,master");
     }
 }
