9c885ce5356248e0ef42d17fe783b8885cf69538
shiwei8b402@126.com <shiwei8b402@126.com>
2020-06-27 14:29:04 +0800

    添加 conflict示例

---
+++ src/main/java/com/ttshixi/git/merge/MergeConflict.java
@@ -0,0 +1,13 @@
+package com.ttshixi.git.merge;
+
+/**
+ * 代码合并冲突示例
+ */
+public class MergeConflict {
+
+    public static void main(String[] args) {
+        String name = "zhangsan";
+        String format = "My name is %s";
+        System.out.println(String.format(format,name));
+    }
+}
