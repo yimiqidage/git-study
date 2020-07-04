b4bf08ea3821fba2d99c1821e60b50339d7f3262
shiwei8b402@126.com <shiwei8b402@126.com>
2020-07-04 15:33:33 +0800

    add my name

--- src/main/java/com/ttshixi/git/merge/CommitBranch.java
+++ src/main/java/com/ttshixi/git/merge/CommitBranch.java
@@ -1,4 +1,8 @@
 package com.ttshixi.git.merge;
 
 public class CommitBranch {
+    public static void main(String[] args) {
+        System.out.println("this is a init branch");
+        System.out.println("add my name -zhangsan");
+    }
 }
